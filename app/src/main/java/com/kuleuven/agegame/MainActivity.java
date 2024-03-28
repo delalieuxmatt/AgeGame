838e73001cd38dabac597d23e5e2ce2eea36ecc1
Matt Delalieux <matt.delalieux@icloud.com>
2024-03-28 15:39:36 +0100

    Some fixes

--- app/src/main/java/com/kuleuven/agegame/MainActivity.java
+++ app/src/main/java/com/kuleuven/agegame/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kuleuven.agegame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,10 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+
+        UserInfo userInfo = new UserInfo(getApplicationContext());
+        if(!userInfo.fileExist()){
+            startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+        }
+
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
     }
