cfd2019b23683c547085bd0d935eca72991c2b89
Matt Delalieux <matt.delalieux@icloud.com>
2024-05-21 23:14:23 +0200

    Need to fix profile page

--- app/src/main/java/com/kuleuven/agegame/imagesPlayed.java
+++ app/src/main/java/com/kuleuven/agegame/imagesPlayed.java
@@ -5,7 +5,6 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
